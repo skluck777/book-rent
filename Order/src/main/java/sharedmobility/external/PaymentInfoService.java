96c09e3a18f6d0bc3aa358c49aa04e6408fc0097
kingtogi07 <kingtogi07@naver.com>
2021-07-12 22:03:24 +0900

    ConfigMMAP

--- Order/src/main/java/sharedmobility/external/PaymentInfoService.java
+++ Order/src/main/java/sharedmobility/external/PaymentInfoService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name="payment", url="http://payment:8080")
+@FeignClient(name="payment", url="http://${api.url.order}")
 public interface PaymentInfoService {
     @RequestMapping(method= RequestMethod.POST, path="/payment")
     public boolean pay(@RequestBody PaymentInfo paymentInfo);
