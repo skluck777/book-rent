2f52e95caa57766ad41e205961e71499575b3d33
skluck777 <85722736+skluck777@users.noreply.github.com>
2021-07-15 10:48:58 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,40 +1,46 @@
 # order
 cd Order
 mvn package
-docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-order:latest .
-docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-order:latest
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-order:v1 .
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-order:v2 .
+docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-order:v1
 
 # payment
 cd ..
 cd Payment
 mvn package
-docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-payment:latest .
-docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-payment:latest
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-payment:v1 .
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-payment:v2 .
+docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-payment:v1
 
 # rent
 cd ..
 cd Rent
 mvn package
-docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-rent:latest .
-docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-rent:latest
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-rent:v1 .
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-rent:v2 .
+docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-rent:v1
 
 # stock
 cd ..
 cd Stock
 mvn package
-docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-stock:latest .
-docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-stock:latest
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-stock:v1 .
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-stock:v2 .
+docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-stock:v1
 
 # dashboard
 cd ..
 cd Dashboard
 mvn package
-docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-dashboard:latest .
-docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-dashboard:latest
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-dashboard:v1 .
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-dashboard:v2 .
+docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-dashboard:v1
 
 # gateway
 cd ..
 cd gateway
 mvn package
-docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-gateway:latest .
-docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-gateway:latest
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-gateway:v1 .
+docker build -t 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-gateway:v2 .
+docker push 879772956301.dkr.ecr.ap-northeast-2.amazonaws.com/user01-gateway:v1
