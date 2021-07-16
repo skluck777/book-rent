f40a605effaa76540ad280d0389e4a1577621ab7
skluck777 <85722736+skluck777@users.noreply.github.com>
2021-07-16 15:26:24 +0900

    Update AbstractEvent.java

--- Payment/src/main/java/cartoonrent/AbstractEvent.java
+++ Payment/src/main/java/cartoonrent/AbstractEvent.java
@@ -1,6 +1,6 @@
-package sharedmobility;
+package cartoonrent;
 
-import sharedmobility.config.kafka.KafkaProcessor;
+import cartoonrent.config.kafka.KafkaProcessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.messaging.MessageChannel;
